19dbaa586f0690eebcdd02510449270c03aff8df
IIrumix <callcenter.cc8gmai.com>
2026-02-06 21:37:23 +0700

    create add grade function

---
+++ add_grade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ระบบคำนวณ GPA</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-[url('asset/bg.png')] inset-0  object-cover size-full">
+  <div class="content-stretch flex flex-col gap-[50px] items-center px-[480px] py-[200px] relative size-full">
+
+  </div>
+</body>
+
+</html>
